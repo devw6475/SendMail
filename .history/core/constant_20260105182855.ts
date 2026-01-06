dd83ae55946411426bc8da9c845825b6deeeeca0
Xdesign <ezechiekouame57@gmail.com>
2026-01-06 00:46:42 -0500

    Remove .history from Git tracking and ignore it

--- .history/core/constant_20260105182855.ts
+++
@@ -1,8 +0,0 @@
-
-export const  menuItems = [
-    { label: 'Accueil', to: '/' },
-    { label: 'Services', to: '/services' },
-    { label: 'Fonctionnalités', to: '/features' },
-    { label: 'Tarifs', to: '/pricing' },
-    { label: 'Contact', to: '/contact' }
-  ]
--- .history/plugins/pinia-persist.client_20260105175038.ts
+++
@@ -1,9 +0,0 @@
-// plugins/pinia-persist.client.ts
-import { defineNuxtPlugin } from '#app'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import type { Pinia } from 'pinia'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    const pinia = nuxtApp.$pinia as Pinia
-    pinia.use(piniaPluginPersistedstate)
-})

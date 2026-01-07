7da89acbb9576eb45fd8855905bc05ae66d2dc32
Xdesign <ezechiekouame57@gmail.com>
2026-01-07 15:18:35 -0500

    update language fix-fr

--- .history/utils/crypto.client_20260107000544.ts
+++
@@ -1,8 +0,0 @@
-import CryptoJS from 'crypto-js'
-
-export function encryptData(data: any, secret: string) {
-  return CryptoJS.AES.encrypt(
-    JSON.stringify(data),
-    secret
-  ).toString()
-}
